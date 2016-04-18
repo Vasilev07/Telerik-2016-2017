356cfad59149698f500d3bd27cbc757a0055c8fd
George <georgevasilev07@gmail.com>
2016-04-18 19:06:11 +0300

    ss

--- C# Homework/ConsoleInAndOut/SumOfFiveNumbers/SumOfFiveNumbers.cs
+++ C# Homework/ConsoleInAndOut/SumOfFiveNumbers/SumOfFiveNumbers.cs
@@ -7,6 +7,13 @@ namespace SumOfFiveNumbers
     {
         static void Main()
         {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+            int sum = a + b + c + d + e;
+            Console.WriteLine(sum);
         }
     }
 }
